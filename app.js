7fe8a7bbf1563f5a4c9602099c329d222d9d8dc1
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-25 15:51:50 -0500

    updated and fixed bug

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
 var { mongoConnect } = require("./mongo.js");
 mongoConnect();
-
-var authRouter = require("./routes/auth");
-app.use("/auth", authRouter);
 
 //enable cors
 const cors = require("cors");
@@ -32,6 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+
+// npm i bcryptjs uuidv4
+// bcrypt gives us access to password generation and authentication funcionality for logging users in to our application.
 const bcrypt = require("bcryptjs");
 const { uuid } = require("uuidv4");
 const { blogsDB } = require("../mongo");
@@ -8,7 +11,9 @@ const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 dotenv.config();
 
+// implement the user creation route using the bcrypt salt/hash functions to take our new users password and encrypt it before saving it to the database.
 const createUser = async (username, passwordHash) => {
+  // Note: You do not have to create the users collection in mongodb before saving to it. Mongo will automatically create the users collection upon insert of a new document.
   const collection = await blogsDB().collection("users");
 
   const user = {
@@ -16,13 +21,14 @@ const createUser = async (username, passwordHash) => {
     password: passwordHash,
     uid: uuid(), // uid stands for User ID. This will be a unique string that we can use to identify our user.
   };
-
+  console.log("mongo response " + collection);
   try {
     // Save user functionality
     await collection.insertOne(user);
     return true;
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    console.log("something is wrong");
+    console.error(error);
     return false;
   }
 };
@@ -35,6 +41,7 @@ router.post("/register-user", async (req, res, next) => {
     const salt = await bcrypt.genSalt(saltRounds);
     const hash = await bcrypt.hash(password, salt);
     const userSaveSuccess = await createUser(username, hash);
+    // console.log(userSaveSuccess);
     res.json({ success: userSaveSuccess }).status(200);
   } catch (e) {
     res.json({ success: false }).status(500);
@@ -42,24 +49,31 @@ router.post("/register-user", async (req, res, next) => {
 });
 
 router.post("/login-user", async (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const collection = await blogsDB().collection("users");
   try {
-    const collection = await blogsDB().collection("users");
     const user = await collection.findOne({
-      username: req.body.username,
+      username: username,
     });
+    if (!user) {
+      res.json({ success: false }).status(204);
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      const jwtSecretKey = process.env.JWT_SECRET_KEY;
+      const data = {
+        time: new Date(),
+        userId: user.uid, // Note: Double check this line of code to be sure that user.uid is coming from your fetched mongo user
+      };
 
-    const match = await bcrypt.compare(req.body.password, user.password);
-
-    const jwtSecretKey = process.env.JWT_SECRET_KEY;
-    const data = {
-      time: new Date(),
-      userId: user.uid, // Note: Double check this line of code to be sure that user.uid is coming from your fetched mongo user
-    };
-    const token = jwt.sign(data, jwtSecretKey);
-
-    res.json({ success: true, token }).status(200);
-  } catch (e) {
-    res.json({ success: false }).status(500);
+      const token = jwt.sign(data, jwtSecretKey);
+      res.json({ success: true, token }).status(200);
+      return;
+    }
+    res.json({ success: false });
+  } catch (error) {
+    res.json({ message: "Error Logging In.", success: false }).status(500);
   }
 });
 
@@ -69,8 +83,8 @@ router.get("/validate-token", async (req, res, next) => {
 
   try {
     const token = req.header(tokenHeaderKey);
-
     const verified = jwt.verify(token, jwtSecretKey);
+
     if (verified) {
       return res.json({ success: true });
     } else {
@@ -79,7 +93,7 @@ router.get("/validate-token", async (req, res, next) => {
     }
   } catch (error) {
     // Access Denied
-    return res.status(401).json({ success: true, message: String(error) });
+    return res.status(401).json({ success: error, message: String(error) });
   }
 });
 
